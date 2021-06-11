dc54d819d757f5aad3ed11d794f5b75e4d469ebc
Julian Texo <JulianTexo@gmail.com>
2021-06-11 11:19:00 +0200

    initial commit

---
+++ commands/reactionrole.js
@@ -0,0 +1,60 @@
+module.exports = {
+    name: 'reactionrole',
+    descritpion: "Sets up a reaction role message!",
+    async execute(message, args, Discord, bot) {
+      const rereg = (args[0] === "registerer");
+      const channel = '838745116420210729';
+      const Ln1Role = message.guild.roles.cache.find(role => role.name === "LN1");
+      const Ln2Role = message.guild.roles.cache.find(role => role.name === "LN2");
+  
+      const Ln1Emoji = '1️⃣';
+      const Ln2Emoji = '2️⃣';
+  
+  if(!rereg){
+      let embed = new Discord.MessageEmbed()
+        .setColor('#ffff00')
+        .setTitle('Choose the role of the game you run!')
+        .setDescription(`${Ln1Emoji} for Little Nightmares 1 role\n` +
+          `${Ln2Emoji} for Little Nightmares 2 role`);    
+      let messageEmbed = await message.channel.send(embed);
+      messageEmbed.react(Ln1Emoji);
+      messageEmbed.react(Ln2Emoji);
+  }
+  
+      bot.on('messageReactionAdd', async (reaction, user) => {
+        if (reaction.message.partial) await reaction.message.fetch();
+        if (reaction.partial) await reaction.fetch();
+        if (user.bot) return;
+        if (!reaction.message.guild) return;
+        if (reaction.message.channel.id == channel) {
+          if (reaction.emoji.name === Ln1Emoji) {
+            await reaction.message.guild.members.cache.get(user.id).roles.add(Ln1Role);
+          }
+          if (reaction.emoji.name === Ln2Emoji) {
+            await reaction.message.guild.members.cache.get(user.id).roles.add(Ln2Role);
+          }
+        } else {
+          return;
+        }
+      });
+  
+      bot.on('messageReactionRemove', async (reaction, user) => {
+        if (reaction.message.partial) await reaction.message.fetch();
+        if (reaction.partial) await reaction.fetch();
+        if (user.bot) return;
+        if (!reaction.message.guild) return;
+        if (reaction.message.channel.id == channel) {
+          if (reaction.emoji.name === Ln1Emoji) {
+            await reaction.message.guild.members.cache.get(user.id).roles.remove(Ln1Role);
+          }
+          if (reaction.emoji.name === Ln2Emoji) {
+            await reaction.message.guild.members.cache.get(user.id).roles.remove(Ln2Role);
+          }
+        } else {
+          return;
+        }
+      });
+    }
+  
+  
+  }
---
+++ lnbot.js
@@ -0,0 +1,46 @@
+const discord = require('discord.js');
+
+const fetch = require("node-fetch");
+
+const commandsChannel = '838745116420210729';
+const logChannel = '841083910401556490';
+
+const prefix = '!';
+
+const fs = require('fs');
+
+const welcome = require('./actions/welcome');
+
+const bot = new discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+bot.commands = new discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    bot.commands.set(command.name, command);
+}
+
+bot.on('message', (message) => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command == 'ping'){
+        bot.commands.get('ping').execute(message, args);
+    }
+    if(command == 'reactionrole'){
+        bot.commands.get('reactionrole').execute(message, args, discord, bot);
+    }    
+});
+
+bot.on('ready', () => {
+  console.log('LnBot is online!');
+  bot.channels.cache.get(logChannel).send("Registering Reaction Listeners...").then(sent => {
+    bot.commands.get('reactionrole').execute(sent, ["registerer"], discord, bot);
+  });
+  welcome(bot);
+});
+
+bot.login('ODM2NTc2MTU2MjU2OTYwNTcy.YIgAQg.kC9Hh6wS5ZgSnF0K-O1ZHsf5-2A');
